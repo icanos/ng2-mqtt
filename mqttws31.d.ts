8a62f76085d11bb44ad2a51f646cf9d3e4c1f05a
Marcus Westin <westin@Marcuss-MacBook-Pro.local>
2016-07-08 11:53:17 +0200

    initial commit

---
+++ mqttws31.d.ts
@@ -0,0 +1,38 @@
+export declare module Paho {
+    export module MQTT {
+        export class Client {
+            constructor(host: string, port: number, clientId: string);
+            constructor(host: string, port: number, path: string, clientId: string);
+            connect(connectOptions: Object):void;
+            subscribe(filter: string, subscribeOptions: Object):void;
+            unsubscribe(filter: string, unsubscribeOptions: Object):void;
+            send(topic: string, payload: string, qos: number, retained: boolean):void;
+            disconnect():void;
+            getTraceLog():void;
+            startTrace():void;
+            stopTrace():void;
+            isConnected():boolean;
+
+            onConnectionLost(responseObject: Object):void;
+            onMessageDelivered():void;
+            onMessageArrived(message: Message):void;
+        }
+
+        export class Message {
+            payloadString(): string;
+            payloadBytes(): Array<any>;
+
+            destinationName(): string;
+            destinationName(newDestinationName: string);
+
+            qos(): number;
+            qos(newQos: number);
+
+            retained(): boolean;
+            retained(newRetained: boolean);
+
+            duplicate(): boolean;
+            duplicate(newDuplicate: boolean);
+        }
+    }
+}
